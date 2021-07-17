af24a285d0ad95ed4a9d5133f636475fcc75fa99
Ace <1kingdom4all@gmail.com>
2021-07-17 14:56:59 -0400

    Adding Player Script

---
+++ Scripts/Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public class Player : KinematicBody2D
+{
+	private int score = 0;
+	
+	private int speed = 20;
+	private int jumpForce = 600;
+	private int gravity = 800;
+	
+	Vector2 vel = new Vector2();
+	
+	private Sprite sprite;
+	
+	public override void _Ready()
+	{
+		sprite = GetNode<Sprite>("Sprite");
+	}
+	
+	public override void _PhysicsProcess(float delta)
+	{
+		vel.y += delta * gravity;
+		
+		if(Input.IsActionPressed("move_left"))
+		{
+			vel.x = -speed;
+		}
+		else if(Input.IsActionPressed("move_right"))
+		{
+			vel.x = speed;
+		}
+		else
+		{
+			vel.x = 0;
+		}
+		
+		var motion = vel * delta;
+		MoveAndCollide(motion);
+	}
+}
