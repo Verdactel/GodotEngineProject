d55750343a9ac968b1f90284997a0ea00bd58933
Ace <1kingdom4all@gmail.com>
2021-07-24 16:33:55 -0400

    Added background scrolling

---
+++ Scripts/backgroundScroll.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class backgroundScroll : Sprite
+{
+    private const float VELOCITY = -2f;
+    private float g_texture_width = 0;
+    public override void _Ready()
+    {
+        g_texture_width = Texture.GetSize().x * Scale.x;
+    }
+
+//  Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+       //MoveLocalX(VELOCITY * delta, false);
+       Position += new Vector2(VELOCITY, 0);
+       _Reposition();
+    }
+
+    public void _Reposition()
+    {
+        if (Position.x < -g_texture_width)
+        {
+            Position += new Vector2(2 * g_texture_width, 0);
+        }
+    }
+}
